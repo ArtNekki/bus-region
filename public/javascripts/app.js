009d2144bd1374e66ad335e45565f0f4096d13c0
Anton Mayatskiy <worknekki@gmail.com>
2019-09-04 12:14:57 +1000

    Animation updated

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,17 +1,66 @@
+function startAnimation() {
+  var elements = $('.animated');
+
+  elements.each(function()  {
+    var elem = $(this);
+
+    var animation = elem.data('animation');
+    var delay = elem.data('animation-delay');
+
+    if(elem.isOnScreen()) {
+      elem.addClass(animation);
+
+      if(delay) {
+        elem.css('animation-delay', delay + 's');
+      }
+
+      elem.on('animationstart', function() {
+        $(this).css('visibility', 'visible');
+      })
+    }
+  })
+}
+
 $(document).ready(function() {
 
+  // viewport detecting
+  $.fn.isOnScreen = function(){
+    var win = $(window);
+    var viewport = {
+        top : win.scrollTop(),
+        left : win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+  };
+
+  startAnimation();
+
+  $(document).on('scroll', function(e) {
+    startAnimation();
+  });
+
+  // scroll to
   $('[data-scroll]').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      target.addClass('animated', 'heartBeat');
-      target.addClass('delay-1s');
-      target.addClass('heartBeat');
+      target.addClass('shake');
+      target.addClass('animated');
+
+      target.on('animationstart', function() {
+        $(this).css('visibility', 'visible');
+      })
 
       target.on('animationend', function() {
-        target.removeClass('animated', 'heartBeat');
-        target.removeClass('delay-1s');
-        target.removeClass('heartBeat');
+        target.removeClass('shake');
+        target.removeClass('animated');
       });
 
       if (target.length) {
@@ -23,6 +72,7 @@ $(document).ready(function() {
     }
   });
 
+  // Change value in select
   $(document).on('change', '.select--form', function() {
     var nextSelect = $(this).parent().next();
 
@@ -31,6 +81,7 @@ $(document).ready(function() {
     }
   });
 
+  // Change value in input
   $('#place-count').on('input', function() {
     var isVal = $(this).val().length;
     var btn = $(this).parents('.form-main').find('[type="submit"]');
@@ -46,6 +97,7 @@ $(document).ready(function() {
     $('html').addClass('page--modal-opened');
   });
 
+  // modal opening
   $(document).on('click', function(e) {
 
     if($(e.target).attr('data-close-modal')) {
